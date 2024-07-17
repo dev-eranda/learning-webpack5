a50e1549f5a3aa1b05ecfbc47ecc19c9e44b6d24
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-07-17 11:34:08 +0530

    init

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Button from './components/button/Button'
-import Heading from './components/heading/Heading'
-import _ from 'lodash'
-
-const heading = new Heading()
-heading.render(_.upperFirst('index'))
-
-const button = new Button()
-button.render()
